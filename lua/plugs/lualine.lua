52b9ba9578280854c94f19e8909fe5ec5acf40b7
Ashis-007 <ashisjc@gmail.com>
2022-09-02 23:39:15 +0530

    modify lualine: remove arrows and useless data

--- lua/plugs/lualine.lua
+++ lua/plugs/lualine.lua
@@ -2,8 +2,10 @@ require('lualine').setup({
   options = {
     icons_enabled = true,
     theme = 'onedark',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    --[[ component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' }, ]]
+    section_separators = '',
+    component_separators = '',
     disabled_filetypes = { "NvimTree" },
     always_divide_middle = true,
     globalstatus = false,
@@ -19,7 +21,7 @@ require('lualine').setup({
         colored = true
       }
     },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_x = { 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' }
   },
